599c2aaf9356a9b3a33099259451876f4c285c33
IrinaShkviro <irinashkviro13@gmail.com>
2015-09-29 12:58:20 +0300

    Clocks are ready

---
+++ Menu/Assets/MainMenu/MM.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MM : MonoBehaviour 
+{
+	public Texture btnTexture;
+	public GUISkin menuSkin;
+	void OnGUI() 
+	{
+//		if (GUI.Button(new Rect(10, 10, 100, 100), btnTexture)) {
+	//		Application.LoadLevel(1);
+//		}
+	}
+}
