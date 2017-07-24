44d97b9d66311c13c42921153c8cee89c20fe10a
IrinaShkviro <irka13-01@mail.ru>
2017-07-24 14:12:32 +0300

    spawning objects

---
+++ EPAM/Spawner/Assets/Scripts/HeroMover.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroMover : MonoBehaviour {
+
+	private float moveSpeed = 10f;
+	private float rotateSpeed = 100f;
+
+	private float bulletSpeed = 500f;
+
+	[SerializeField]
+	private GameObject bulletPrefab;
+
+	[SerializeField]
+	private Transform bulletSpawnPoint;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	private void Fire() {
+		GameObject bulletClone = Instantiate(bulletPrefab) as GameObject;
+		bulletClone.transform.parent = gameObject.transform;
+		bulletClone.transform.localPosition = new Vector3 (0.5f, 0.5f, 1);
+		Rigidbody bulletBody = bulletClone.GetComponent<Rigidbody> (); 
+		bulletBody.velocity = gameObject.transform.forward * bulletSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += transform.forward * Time.deltaTime * moveSpeed;
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position -= transform.forward * Time.deltaTime * moveSpeed;
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate(Vector3.up, -Time.deltaTime * rotateSpeed);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate(Vector3.up, Time.deltaTime * rotateSpeed);
+		}
+
+		// Calls the fire method when holding down ctrl or mouse
+		if (Input.GetButtonDown("Fire1")) {
+			Fire();
+		}
+	}
+}
