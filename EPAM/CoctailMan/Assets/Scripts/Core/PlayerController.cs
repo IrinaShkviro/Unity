08714be3a827b6f6720ae8f35a60846f225a1f4a
IrinaShkviro <irinashkviro13@gmail.com>
2017-07-27 00:07:25 +0300

    add player controller

--- EPAM/CoctailMan/Assets/Scripts/Core/PlayerController.cs
+++ EPAM/CoctailMan/Assets/Scripts/Core/PlayerController.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private GameObject player;
+    private Rigidbody playerBody;
+
+    [SerializeField]
+    private int speed = 6;
+    [SerializeField]
+    private int rotationSpeed = 250;
+    [SerializeField]
+    private int jumpHigh = 3;
+
+    void Start()
+    {
+        player = (GameObject)this.gameObject;
+        playerBody = player.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            playerBody.AddForce(transform.forward * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            playerBody.AddForce(-transform.forward * speed);
+        }
+        
+		if (Input.GetKey (KeyCode.A)) {
+            playerBody.AddTorque(-transform.up * rotationSpeed/10);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			playerBody.AddTorque(transform.up * rotationSpeed/10);
+		}
+
+		// Calls the fire method when holding down ctrl or mouse
+		if (Input.GetButtonDown("Fire1")) {
+			//Fire();
+		}
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    }
 }
